8fb2b103ebd1e62ba561d1aa25c101a7ec3f30ce
Taohid Hossain <htaohid31@gmail.com>
2024-08-16 21:45:46 +0600

    add stubs

--- CPP/LinkedList/main.cpp
+++ CPP/LinkedList/main.cpp
@@ -36,7 +36,7 @@ public:
         }
     protected:
         Node *current;
-        Object retrive() const{
+        Object& retrive() const{
             return current->data;
         }
         const_iterator(Node *p): current{p}
@@ -65,15 +65,98 @@ public:
         iterator(Node *p): const_iterator{p} {}
         friend class List<Object>;
     };
+public:
+    List(){
+    }
+    List(const List& rhs){
+    }
+    List(List&& rhs){
+    }
+    ~List(){
+    }
+    List& operator=(const List& rhs){
+    }
+    List& operator=(List&& rhs){
+    }
 
+    iterator begin(){
+
+    }
+    const_iterator begin() const{
+
+    }
+    iterator end(){
+
+    }
+    const_iterator end() const{
+
+    }
+
+    int size() const{
+
+    }
+    bool empty() const {
+
+    }
+
+    void clear(){
+
+    }
+
+    Object& front(){
+
+    }
+    const Object& front() const{
+
+    }
+    Object& back(){
+
+    }
+    const Object& back() const{
+
+    }
+    void push_front(const Object& x){
+
+    }
+    void push_front(Object&& x){
+
+    }
+    void push_back(Object& x){
+
+    }
+    void push_back(Object&& x){
+
+    }
+    void pop_front(){
+
+    }
+    void pop_back(){
+
+    }
+
+    iterator insert(iterator itr, const Object& x){
+
+    }
+    iterator insert(iterator itr, Object&& x){
+
+    }
+    iterator erase(iterator itr){
+
+    }
+    iterator erase(iterator from, iterator to){
+
+    }
 
 private:
     int theSize;
     Node *head;
     Node *tail;
+    void init(){
+
+    }
 };
 int main(){
     //cout << "fafasf";
     return 0;
 }
-
+// git add ./CPP/LinkedList
