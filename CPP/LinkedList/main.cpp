c02b1c8f2d006e8593c15d94456da09348992d9c
Taohid Hossain <htaohid31@gmail.com>
2024-08-16 10:39:08 +0600

    add const_iterator and iterator friend classes

--- CPP/LinkedList/main.cpp
+++ CPP/LinkedList/main.cpp
@@ -9,30 +9,30 @@ private:
         Node *prev;
         Node *next;
 
-        Node(const Object& d = Object{}, Node *p = nullptr, Node *n = nullptr): data{d}, prev{p}, next{n}
-        {}
-        Node(const Object&& d, Node *p = nullptr, Node *n = nullptr): data{move(d}, prev{p}, next{n}
-        {}
+        Node(const Object& d = Object{}, Node *p = nullptr, Node *n = nullptr): data{d}, prev{p}, next{n} {}
+        Node(const Object&& d, Node *p = nullptr, Node *n = nullptr): data{move(d)}, prev{p}, next{n} {}
     };
 public:
     class const_iterator{
     public:
-        const_iterator(): current{nullptr}
-        {}
+        const_iterator(): current{nullptr} {}
         const Object& operator*() const{
             return retrive();
         }
         const_iterator& operator++(){
-
+            current = current->next;
+            return *this;
         }
         const_iterator& operator++(int){
-
+            const_iterator old = *this;
+            ++(*this);
+            return old;
         }
-        bool operator==(const_iterator& rhs) const{
-
+        bool operator==(const const_iterator& rhs) const{
+            return current == rhs.current;
         }
-        bool operator==(const_iterator&& rhs) const{
-
+        bool operator!=(const const_iterator& rhs) const{
+            return !(*this == rhs);
         }
     protected:
         Node *current;
@@ -44,7 +44,26 @@ public:
         friend class List<Object>;
     };
     class iterator: public const_iterator{
-
+    public:
+        iterator(){}
+        Object& operator*(){
+            return const_iterator::retrive();
+        }
+        const Object& operator*() const{
+            return const_iterator::operator*();
+        }
+        iterator& operator++(){
+            this->current = this->current->next;
+            return *this;
+        }
+        iterator& operator++(int){
+            iterator old = *this;
+            ++(*this);
+            return old;
+        }
+    protected:
+        iterator(Node *p): const_iterator{p} {}
+        friend class List<Object>;
     };
 
 
@@ -57,3 +76,4 @@ int main(){
     //cout << "fafasf";
     return 0;
 }
+
